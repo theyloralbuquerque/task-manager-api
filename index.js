9910cf341c271990a13dbabd3a02a309349ea2d7
Theylor Albuquerque <theylor.oliveira@icc.org.br>
2024-12-03 17:46:47 -0300

    feat: add route to create a new task

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const connectTodatabase = require('./src/database/mongoose.database');
+const TaskModel = require('./src/models/task.model');
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 connectTodatabase();
 
-app.get('/', (req, res) => {
-    const tasks = [{ description: 'Estudar', isCompleted: false }];
-    res.status(200).send(tasks);
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await TaskModel.finfind({});
+        res.status(200).send(tasks);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
+});
+
+app.post('/tasks', async (req, res) => {
+    try {
+        const newTask = new TaskModel(req.body);
+
+        await newTask.save();
+
+        res.status(201).send(newTask);
+    } catch (e) {
+        res.status(500).send(e.message);
+    }
 });
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
